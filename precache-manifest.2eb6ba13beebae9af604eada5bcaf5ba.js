6195d5df1483db38cb5f0cc0b7e6d48ad8388459
zzana0219 <zzana0219@naver.com>
2020-08-05 23:03:51 +0900

    Updates

---
+++ precache-manifest.2eb6ba13beebae9af604eada5bcaf5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b31a8958dc8706d9c0a1c86e833be00",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "abd80e50088cf3919ab9",
+    "url": "/movie_app/static/css/main.abba741b.chunk.css"
+  },
+  {
+    "revision": "065f929fd96323f1055d",
+    "url": "/movie_app/static/js/2.9f22f0b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.9f22f0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd80e50088cf3919ab9",
+    "url": "/movie_app/static/js/main.a0e0e405.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
